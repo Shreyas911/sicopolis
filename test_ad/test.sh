6a68e624909d636f4c4ffcfe5d81d2c685dfb302
Shreyas911 <shreyasg911@gmail.com>
2021-08-03 19:13:20 -0500

    Minor fixes to adjust to adjust to docker env.

--- test_ad/test.sh
+++ test_ad/test.sh
@@ -5,23 +5,23 @@ cd ../src
 LISDIR="/lis-2.0.30/installation"
 NETCDF_DIR="/usr"
 @test "verify that the adjoint code is compiling for GRL" {
-	run make -f MakefileTapenade clean; make -f MakefileTapenade driveradjoint HEADER=v5_grl20_ss25ka DOMAIN_SHORT=grl;
+	run make -f MakefileTapenade clean; make -f MakefileTapenade driveradjoint HEADER=v5_grl20_ss25ka DOMAIN_SHORT=grl LISDIR=${LISDIR} NETCDF_FORTRAN_DIR=${NETCDF_FORTRAN_DIR} TRAVIS_CI=yes;
 	[ "$status" -eq 0 ]
 }
 
 @test "verify that the grdchk code is compiling for GRL" {
-        run make -f MakefileTapenade clean; make -f MakefileTapenade drivergrdchk HEADER=v5_grl20_ss25ka DOMAIN_SHORT=grl;
+        run make -f MakefileTapenade clean; make -f MakefileTapenade drivergrdchk HEADER=v5_grl20_ss25ka DOMAIN_SHORT=grl LISDIR=${LISDIR} NETCDF_FORTRAN_DIR=${NETCDF_FORTRAN_DIR} TRAVIS_CI=yes;
         [ "$status" -eq 0 ]
 }
 
 #@test "verify that the results are correct for GRL" {
-#        run make -f MakefileTapenade clean; make -f MakefileTapenade drivergrdchk HEADER=v5_grl20_ss25ka DOMAIN_SHORT=grl;
+#        run make -f MakefileTapenade clean; make -f MakefileTapenade drivergrdchk HEADER=v5_grl20_ss25ka DOMAIN_SHORT=grl LISDIR=${LISDIR} NETCDF_FORTRAN_DIR=${NETCDF_FORTRAN_DIR} TRAVIS_CI=yes;
 #        [ "$status" -eq 0 ]
 #
 #	run ./drivergrdchk
 #	[ "$status" -eq 0 ]
 #
-#        run make -f MakefileTapenade clean; make -f MakefileTapenade driveradjoint HEADER=v5_grl20_ss25ka DOMAIN_SHORT=grl;
+#        run make -f MakefileTapenade clean; make -f MakefileTapenade driveradjoint HEADER=v5_grl20_ss25ka DOMAIN_SHORT=grl LISDIR=${LISDIR} NETCDF_FORTRAN_DIR=${NETCDF_FORTRAN_DIR} TRAVIS_CI=yes;
 #        [ "$status" -eq 0 ]
 #
 #	run ./driveradjoint
@@ -33,12 +33,12 @@ NETCDF_DIR="/usr"
 #}
 
 @test "verify that the adjoint code is compiling for ANT" {
-        run make -f MakefileTapenade clean; make -f MakefileTapenade driveradjoint HEADER=v5_ant64_b2_future09_ctrl DOMAIN_SHORT=ant;
+        run make -f MakefileTapenade clean; make -f MakefileTapenade driveradjoint HEADER=v5_ant64_b2_future09_ctrl DOMAIN_SHORT=ant LISDIR=${LISDIR} NETCDF_FORTRAN_DIR=${NETCDF_FORTRAN_DIR} TRAVIS_CI=yes;
         [ "$status" -eq 0 ]
 }
 
 @test "verify that the grdchk code is compiling for ANT" {
-        run make -f MakefileTapenade clean; make -f MakefileTapenade drivergrdchk HEADER=v5_ant64_b2_future09_ctrl DOMAIN_SHORT=ant;
+        run make -f MakefileTapenade clean; make -f MakefileTapenade drivergrdchk HEADER=v5_ant64_b2_future09_ctrl DOMAIN_SHORT=ant LISDIR=${LISDIR} NETCDF_FORTRAN_DIR=${NETCDF_FORTRAN_DIR} TRAVIS_CI=yes;
         [ "$status" -eq 0 ]
 }
 
