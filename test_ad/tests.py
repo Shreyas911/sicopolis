d31985871c9a79d0094bc5d7c1afd4f37e80dc94
Shreyas911 <shreyasg911@gmail.com>
2021-08-23 10:21:27 -0500

    removed capture_output option which caused errors for some reason.

--- test_ad/tests.py
+++ test_ad/tests.py
@@ -3,10 +3,10 @@ import os
 import subprocess
 
 def test_v5_ant40_ss25ka():
-	subprocess.run('python tapenade_config.py -head v5_ant40_ss25ka -dom ant -dv fc -iv H -delta 1.e-3 --travis', capture_output = True, shell = True, check = True)
+	subprocess.run('python tapenade_config.py -head v5_ant40_ss25ka -dom ant -dv fc -iv H -delta 1.e-3 --travis', shell = True, check = True)
 
 def test_v5_ant64_b2_future09_ctrl():
-	subprocess.run('python tapenade_config.py -head v5_ant64_b2_future09_ctrl -dom ant -dv fc -iv H -delta 1.e-3 --travis', capture_output = True, shell = True, check = True)
+	subprocess.run('python tapenade_config.py -head v5_ant64_b2_future09_ctrl -dom ant -dv fc -iv H -delta 1.e-3 --travis', shell = True, check = True)
 
 def test_v5_grl20_ss25ka():	
-	subprocess.run('python tapenade_config.py -head v5_grl20_ss25ka -dom grl -dv fc -iv H -delta 1.e-3 --travis', capture_output = True, shell = True, check = True)
+	subprocess.run('python tapenade_config.py -head v5_grl20_ss25ka -dom grl -dv fc -iv H -delta 1.e-3 --travis', shell = True, check = True)
