41e7c4c4d1fb02ad422ab75ab3794138285d9e64
Ralf Greve <greve@lowtem.hokudai.ac.jp>
2021-09-20 11:59:01 +0900

    Branch npi: Following the changes in 'develop', changes made in sico_init_m and boundary_m re spatially varying sea level z_sl(j,i).

--- src/subroutines/xyz/npi/sico_init_m.F90
+++ src/subroutines/xyz/npi/sico_init_m.F90
@@ -53,7 +53,7 @@ subroutine sico_init(delta_ts, glac_index, &
                dtime, dtime_temp, dtime_wss, dtime_out, dtime_ser, &
                time, time_init, time_end, time_output, &
                dxi, deta, dzeta_c, dzeta_t, dzeta_r, &
-               z_sl, dzsl_dtau, z_mar, &
+               z_mar, &
                ndat2d, ndat3d, n_output, &
                runname)
 
@@ -100,7 +100,7 @@ real(dp),           intent(out) :: dtime, dtime_temp, dtime_wss, &
                                    dtime_out, dtime_ser
 real(dp),           intent(out) :: time, time_init, time_end, time_output(100)
 real(dp),           intent(out) :: dxi, deta, dzeta_c, dzeta_t, dzeta_r
-real(dp),           intent(out) :: z_sl, dzsl_dtau, z_mar
+real(dp),           intent(out) :: z_mar
 
 character(len=100), intent(out) :: runname
 
@@ -2029,10 +2029,11 @@ end do
 
 call topography1(dxi, deta)
 
-z_sl = -1.11e+11_dp   ! dummy value for initial call of subroutine boundary
+z_sl      = -1.11e+11_dp   ! dummy values for initial call
+z_sl_mean = -1.11e+11_dp   ! of subroutine boundary
 
 call boundary(time_init, dtime, dxi, deta, &
-              delta_ts, glac_index, z_sl, dzsl_dtau, z_mar)
+              delta_ts, glac_index, z_mar)
 
 #if !defined(ALLOW_OPENAD) /* Normal */
 
@@ -2075,7 +2076,7 @@ H_w     = 0.0_dp
 #elif (TEMP_INIT==4)
   call init_temp_water_age_1_4()
 #elif (TEMP_INIT==5)
-  call init_temp_water_age_1_5(z_sl, anfdatname)
+  call init_temp_water_age_1_5(anfdatname)
 #else
   errormsg = ' >>> sico_init: TEMP_INIT must be between 1 and 5!'
   call error(errormsg)
@@ -2115,10 +2116,11 @@ vis_int_g = 0.0_dp
 
 call topography2(dxi, deta)
 
-z_sl = -1.11e+11_dp   ! dummy value for initial call of subroutine boundary
+z_sl      = -1.11e+11_dp   ! dummy values for initial call
+z_sl_mean = -1.11e+11_dp   ! of subroutine boundary
 
 call boundary(time_init, dtime, dxi, deta, &
-              delta_ts, glac_index, z_sl, dzsl_dtau, z_mar)
+              delta_ts, glac_index, z_mar)
 
 as_perp_apl = 0.0_dp
 
@@ -2165,10 +2167,10 @@ vis_int_g = 0.0_dp
 
 #elif (ANF_DAT==3)
 
-call topography3(dxi, deta, z_sl, anfdatname)
+call topography3(dxi, deta, anfdatname)
 
 call boundary(time_init, dtime, dxi, deta, &
-              delta_ts, glac_index, z_sl, dzsl_dtau, z_mar)
+              delta_ts, glac_index, z_mar)
 
 #if !defined(ALLOW_OPENAD) /* Normal */
 
@@ -2245,21 +2247,21 @@ end do
 
 call calc_temp_melt()
 call flag_update_gf_gl_cf()
-call calc_dzs_dxy_aux(z_sl, dxi, deta)
+call calc_dzs_dxy_aux(dxi, deta)
 
 #if (DYNAMICS==1 || DYNAMICS==2)
 
-call calc_vxy_b_sia(time, z_sl)
+call calc_vxy_b_sia(time)
 call calc_vxy_sia(dzeta_c, dzeta_t)
 
 #if (MARGIN==3 || DYNAMICS==2)
-call calc_vxy_ssa(z_sl, dxi, deta, dzeta_c, dzeta_t)
+call calc_vxy_ssa(dxi, deta, dzeta_c, dzeta_t)
 #endif
 
 call calc_vz_grounded(dxi, deta, dzeta_c, dzeta_t)
 
 #if (MARGIN==3)
-call calc_vz_floating(z_sl, dxi, deta, dzeta_c)
+call calc_vz_floating(dxi, deta, dzeta_c)
 #endif
 
 #elif (DYNAMICS==0)
@@ -2357,7 +2359,7 @@ if ((forcing_flag == 1).or.(forcing_flag == 3)) then
    write(12,1102)
    write(12,1103)
 
-   1102 format('         t(a)  D_Ts(deg C)      z_sl(m)',/, &
+   1102 format('         t(a)  D_Ts(deg C) z_sl_mean(m)',/, &
                '                    V(m^3)     V_g(m^3)     V_f(m^3)', &
                '       A(m^2)     A_g(m^2)     A_f(m^2)',/, &
                '                               V_sle(m)     V_t(m^3)', &
@@ -2372,7 +2374,7 @@ else if (forcing_flag == 2) then
    write(12,1112)
    write(12,1113)
 
-   1112 format('         t(a)  glac_ind(1)      z_sl(m)',/, &
+   1112 format('         t(a)  glac_ind(1) z_sl_mean(m)',/, &
                '                    V(m^3)     V_g(m^3)     V_f(m^3)', &
                '       A(m^2)     A_g(m^2)     A_f(m^2)',/, &
                '                               V_sle(m)     V_t(m^3)', &
@@ -2439,7 +2441,7 @@ write(14,'(1x,a)') '---------------------'
 #endif
 
    if (flag_init_output) &
-      call output1(runname, time_init, delta_ts, glac_index, z_sl, &
+      call output1(runname, time_init, delta_ts, glac_index, &
                    flag_3d_output, ndat2d, ndat3d)
 
 #elif (OUTPUT==2)
@@ -2455,7 +2457,7 @@ if (time_output(1) <= time_init+eps) then
    call error(errormsg)
 #endif
 
-   call output1(runname, time_init, delta_ts, glac_index, z_sl, &
+   call output1(runname, time_init, delta_ts, glac_index, &
                 flag_3d_output, ndat2d, ndat3d)
 
 end if
@@ -2465,14 +2467,14 @@ end if
    flag_3d_output = .false.
 
    if (flag_init_output) &
-      call output1(runname, time_init, delta_ts, glac_index, z_sl, &
+      call output1(runname, time_init, delta_ts, glac_index, &
                    flag_3d_output, ndat2d, ndat3d)
 
 if (time_output(1) <= time_init+eps) then
 
    flag_3d_output = .true.
 
-   call output1(runname, time_init, delta_ts, glac_index, z_sl, &
+   call output1(runname, time_init, delta_ts, glac_index, &
                 flag_3d_output, ndat2d, ndat3d)
 
 end if
@@ -2483,8 +2485,8 @@ end if
 #endif
 
 if (flag_init_output) then
-   call output2(time_init, dxi, deta, delta_ts, glac_index, z_sl)
-   call output4(time_init, dxi, deta, delta_ts, glac_index, z_sl)
+   call output2(time_init, dxi, deta, delta_ts, glac_index)
+   call output4(time_init, dxi, deta, delta_ts, glac_index)
 end if
 
 #else /* OpenAD */
@@ -2917,7 +2919,7 @@ end subroutine topography2
 !! (including gradients) and of the horizontal grid spacings dxi, deta.
 !! For initial topography from previous simulation.
 !<------------------------------------------------------------------------------
-subroutine topography3(dxi, deta, z_sl, anfdatname)
+subroutine topography3(dxi, deta, anfdatname)
 
   use read_m, only : read_tms_nc, read_2d_input
 
@@ -2932,7 +2934,7 @@ implicit none
 
 character(len=100), intent(in) :: anfdatname
 
-real(dp),          intent(out) :: dxi, deta, z_sl
+real(dp),          intent(out) :: dxi, deta
 
 integer(i4b) :: i, j, n
 
@@ -2942,7 +2944,7 @@ real(dp), dimension(0:JMAX,0:IMAX) :: field2d_aux
 
 !-------- Read data from time-slice file of previous simulation --------
 
-call read_tms_nc(z_sl, anfdatname)
+call read_tms_nc(anfdatname)
 
 !-------- Read topography of the relaxed bedrock --------
 
