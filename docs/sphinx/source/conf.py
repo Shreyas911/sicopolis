03e8269195d066c4664d6718382a572a9683c02c
Shreyas911 <shreyasg911@gmail.com>
2022-04-15 17:50:14 -0500

    Minor debugging

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -27,7 +27,7 @@ author = 'Ralf Greve, Shreyas Sunil Gaikwad, Sri Hari Krishna Narayanan, Laurent
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['nbsphinx', 'IPython.sphinxext.ipython_console_highlighting', 'sphinx.ext.intersphinx']
+extensions = ['nbsphinx', 'sphinxext.ipython_console_highlighting', 'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
