02b6f42a07e0ddc59c0f539caa9b4d8e013993e1
Shreyas911 <shreyasg911@gmail.com>
2022-05-16 20:22:57 -0500

    Using validation and inputs.json for pytests.

--- test_ad/tests.py
+++ test_ad/tests.py
@@ -2,22 +2,23 @@ import pytest
 import os
 import subprocess
 
+
 def test_v5_ant40_ss25ka_H():
 	subprocess.run (['cp', '../runs/headers/sico_specs_v5_ant40_ss25ka.h', '../src/sico_specs.h'])
-	subprocess.run('python3 tapenade_config.py -head v5_ant40_ss25ka -dom ant -dv fc -iv H -delta 1.e-3 --travis', shell = True, check = True)
+	subprocess.run('python3 tapenade_config.py -head v5_ant40_ss25ka -dom ant -jsf inputs.json', shell = True, check = True)
 
 def test_v5_ant64_b2_future09_ctrl_H():
 	subprocess.run (['cp', '../runs/headers/sico_specs_v5_ant64_b2_future09_ctrl.h', '../src/sico_specs.h'])
-	subprocess.run('python3 tapenade_config.py -head v5_ant64_b2_future09_ctrl -dom ant -dv fc -iv H -delta 5.e-2 --travis', shell = True, check = True)
+	subprocess.run('python3 tapenade_config.py -head v5_ant64_b2_future09_ctrl -dom ant -delta 5.e-2 -jsf inputs.json', shell = True, check = True)
 
 def test_v5_grl20_ss25ka_H():	
 	subprocess.run (['cp', '../runs/headers/sico_specs_v5_grl20_ss25ka.h', '../src/sico_specs.h'])
-	subprocess.run('python3 tapenade_config.py -head v5_grl20_ss25ka -dom grl -dv fc -iv H -delta 1.e-3 --travis', shell = True, check = True)
+	subprocess.run('python3 tapenade_config.py -jsf inputs.json', shell = True, check = True)
 
 def test_v5_ant40_ss25ka_q_geo():
 	subprocess.run (['cp', '../runs/headers/sico_specs_v5_ant40_ss25ka.h', '../src/sico_specs.h'])
-	subprocess.run('python3 tapenade_config.py -head v5_ant40_ss25ka -dom ant -dv fc -iv q_geo -delta 1.e-3 --travis', shell = True, check = True)
+	subprocess.run('python3 tapenade_config.py -head v5_ant40_ss25ka -dom ant -iv q_geo -jsf inputs.json', shell = True, check = True)
 
 def test_v5_grl20_ss25ka_q_geo():
         subprocess.run (['cp', '../runs/headers/sico_specs_v5_grl20_ss25ka.h', '../src/sico_specs.h'])
-        subprocess.run('python3 tapenade_config.py -head v5_grl20_ss25ka -dom grl -dv fc -iv q_geo -delta 1.e-3 --travis', shell = True, check = True)
+        subprocess.run('python3 tapenade_config.py -iv q_geo -jsf inputs.json', shell = True, check = True)
