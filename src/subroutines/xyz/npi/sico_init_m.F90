05afa460efa091bcc894e364de0aa7601956c61e
Ralf Greve <greve@lowtem.hokudai.ac.jp>
2022-05-31 11:46:43 +0900

    Branch 'npi': Following the changes in 'develop', module sico_init_m updated.

--- src/subroutines/xyz/npi/sico_init_m.F90
+++ src/subroutines/xyz/npi/sico_init_m.F90
@@ -295,6 +295,8 @@ call error(errormsg)
 
 #if !defined(ALLOW_OPENAD) /* Normal */
 
+#if (!defined(CHECK_RES_IMAX_JMAX) || CHECK_RES_IMAX_JMAX==1)
+
 #if (GRID==0)
 
 if (approx_equal(DX, 0.45_dp, eps_sp_dp)) then
@@ -343,6 +345,15 @@ errormsg = ' >>> sico_init: GRID==2 not allowed for this application!'
 call error(errormsg)
 
 #endif
+
+#else /* CHECK_RES_IMAX_JMAX==0 */
+
+write(6, fmt='(a)') ' >>> sico_init: CHECK_RES_IMAX_JMAX==0'
+write(6, fmt='(a)') '      -> compatibility check between horizontal resolution'
+write(6, fmt='(a)') '         and number of grid points not performed.'
+write(6, fmt='(a)') ' '
+
+#endif /* CHECK_RES_IMAX_JMAX */
 
 #else /* OpenAD */
 
@@ -733,6 +744,11 @@ errormsg = ' >>> sico_init: GRID==2 not allowed for this application!'
 call error(errormsg)
 #endif
 write(10, fmt=trim(fmt1)) ' '
+
+#if (defined(CHECK_RES_IMAX_JMAX))
+write(10, fmt=trim(fmt2)) 'CHECK_RES_IMAX_JMAX = ', CHECK_RES_IMAX_JMAX
+write(10, fmt=trim(fmt1)) ' '
+#endif
 
 write(10, fmt=trim(fmt3)) 'year_zero  =', year_zero
 write(10, fmt=trim(fmt3)) 'time_init  =', time_init0
