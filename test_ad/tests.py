feb6b36459ee943d508da300d2e03e25427824fd
Shreyas911 <shreyasg911@gmail.com>
2022-07-18 01:49:16 -0500

    Minor correction to AD tests.

--- test_ad/tests.py
+++ test_ad/tests.py
@@ -4,8 +4,8 @@ import subprocess
 
 
 def test_v5_ant40_b2_ss25ka_H():
-	subprocess.run (['cp', '../runs/headers/sico_specs_v5_ant40_ss25ka.h', '../src/sico_specs.h'])
-	subprocess.run('python3 tapenade_config.py -head v5_ant40_ss25ka -dom ant -jsf inputs.json', shell = True, check = True)
+	subprocess.run (['cp', '../runs/headers/sico_specs_v5_ant40_b2_ss25ka.h', '../src/sico_specs.h'])
+	subprocess.run('python3 tapenade_config.py -head v5_ant40_b2_ss25ka -dom ant -jsf inputs.json', shell = True, check = True)
 
 def test_v5_ant64_b2_future09_ctrl_H():
 	subprocess.run (['cp', '../runs/headers/sico_specs_v5_ant64_b2_future09_ctrl.h', '../src/sico_specs.h'])
@@ -16,8 +16,8 @@ def test_v5_grl16_bm4_ss25ka_H():
 	subprocess.run('python3 tapenade_config.py -jsf inputs.json', shell = True, check = True)
 
 def test_v5_ant40_b2_ss25ka_q_geo():
-	subprocess.run (['cp', '../runs/headers/sico_specs_v5_ant40_ss25ka.h', '../src/sico_specs.h'])
-	subprocess.run('python3 tapenade_config.py -head v5_ant40_ss25ka -dom ant -iv q_geo -jsf inputs.json', shell = True, check = True)
+	subprocess.run (['cp', '../runs/headers/sico_specs_v5_ant40_b2_ss25ka.h', '../src/sico_specs.h'])
+	subprocess.run('python3 tapenade_config.py -head v5_ant40_b2_ss25ka -dom ant -iv q_geo -jsf inputs.json', shell = True, check = True)
 
 def test_v5_grl16_bm4_ss25ka_q_geo():
         subprocess.run (['cp', '../runs/headers/sico_specs_v5_grl16_bm4_ss25ka.h', '../src/sico_specs.h'])
