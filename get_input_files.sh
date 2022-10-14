a1edd9a588c08eddd5a5da0333955dc660d324d6
Ralf Greve <greve@lowtem.hokudai.ac.jp>
2022-10-14 15:07:29 +0900

    URL for Zenodo repository in script get_input_files.sh updated.

--- get_input_files.sh
+++ get_input_files.sh
@@ -5,7 +5,7 @@
 #   Downloading the input files for SICOPOLIS,
 #   copying them to the corresponding directories.
 #   
-#    - Ralf Greve, 2022-06-30.
+#    - Ralf Greve, 2022-10-14.
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 #-------- Settings (to be customized) --------
@@ -36,7 +36,7 @@ MOCHO_FLAG=1     # Mocho-Choshuenco ice cap:
 #-------- Initialization --------
 
 # Zenodo repo:
-REPO_URL=https://zenodo.org/record/6782657/files
+REPO_URL=https://zenodo.org/record/7197105/files
 
 # Backup repo:
 # REPO_URL=http://wwwice.lowtem.hokudai.ac.jp/repo/sicopolis/sico_in
