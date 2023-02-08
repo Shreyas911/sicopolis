aa692fbe6da3b714320787bf907acf4402763bed
Ralf Greve <greve@lowtem.hokudai.ac.jp>
2023-02-08 19:22:54 +0900

    Tools make_ismip_output, resolution_doubler: Option NETCDF4_ENABLED==1 now produces output in netCDF-4 classic model rather than full netCDF-4.

--- tools/make_ismip_output/make_ismip_output.F90
+++ tools/make_ismip_output/make_ismip_output.F90
@@ -9,7 +9,7 @@
 !!
 !! @section Date
 !!
-!! 2023-01-03
+!! 2023-02-08
 !!
 !! @section Copyright
 !!
@@ -1085,7 +1085,7 @@ stop ' >>> init_ismip_netcdf: TIME_UNIT must be either 1 or 2!'
 #endif
 
 #if (NETCDF4_ENABLED==1)
-  cmode           = NF90_NETCDF4
+  cmode           = ior(NF90_NETCDF4, NF90_CLASSIC_MODEL)
   n_deflate_level = 1
   flag_shuffle    = .true.
 #else
