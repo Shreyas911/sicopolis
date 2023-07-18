d149320336fe14ee2c837beea6e3e7c656fcb435
Shreyas911 <shreyasg911@gmail.com>
2023-07-18 00:14:30 -0500

    Run-specs headers in the repository renamed from sico_specs_v5_*.h to sico_specs_repo_*.h for the AD-related files too.

--- test_ad/tests.py
+++ test_ad/tests.py
@@ -5,11 +5,11 @@ import subprocess
 
 def test_repo_ant40_b2_ss25ka_H():
 	subprocess.run (['cp', '../runs/headers/sico_specs_repo_ant40_b2_ss25ka.h', '../src/sico_specs.h'])
-	subprocess.run('python3 tapenade_config.py -head v5_ant40_b2_ss25ka -dom ant -jsf inputs.json', shell = True, check = True)
+	subprocess.run('python3 tapenade_config.py -head repo_ant40_b2_ss25ka -dom ant -jsf inputs.json', shell = True, check = True)
 
 def test_repo_ant64_b2_future09_ctrl_H():
 	subprocess.run (['cp', '../runs/headers/sico_specs_repo_ant64_b2_future09_ctrl.h', '../src/sico_specs.h'])
-	subprocess.run('python3 tapenade_config.py -head v5_ant64_b2_future09_ctrl -dom ant -delta 5.e-2 -jsf inputs.json', shell = True, check = True)
+	subprocess.run('python3 tapenade_config.py -head repo_ant64_b2_future09_ctrl -dom ant -delta 5.e-2 -jsf inputs.json', shell = True, check = True)
 
 def test_repo_grl16_bm5_ss25ka_H():	
 	subprocess.run (['cp', '../runs/headers/sico_specs_repo_grl16_bm5_ss25ka.h', '../src/sico_specs.h'])
@@ -17,7 +17,7 @@ def test_repo_grl16_bm5_ss25ka_H():
 
 def test_repo_ant40_b2_ss25ka_q_geo():
 	subprocess.run (['cp', '../runs/headers/sico_specs_repo_ant40_b2_ss25ka.h', '../src/sico_specs.h'])
-	subprocess.run('python3 tapenade_config.py -head v5_ant40_b2_ss25ka -dom ant -iv q_geo -jsf inputs.json', shell = True, check = True)
+	subprocess.run('python3 tapenade_config.py -head repo_ant40_b2_ss25ka -dom ant -iv q_geo -jsf inputs.json', shell = True, check = True)
 
 def test_repo_grl16_bm5_ss25ka_q_geo():
         subprocess.run (['cp', '../runs/headers/sico_specs_repo_grl16_bm5_ss25ka.h', '../src/sico_specs.h'])
