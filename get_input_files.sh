e2555f71f24c4793918bef8b09d336142964f24e
Ralf Greve <greve@lowtem.hokudai.ac.jp>
2023-09-20 15:38:12 +0900

    URL for Zenodo repository in script get_input_files.sh updated (Aq1 geothermal heat flux for Antarctica added).

--- get_input_files.sh
+++ get_input_files.sh
@@ -9,7 +9,7 @@
 #   copying them to the corresponding directories.
 #
 # Author: Ralf Greve
-# Date:   2023-09-11
+# Date:   2023-09-20
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 #-------- Settings (to be customized) --------
@@ -40,7 +40,7 @@ MOCHO_FLAG=1     # Mocho-Choshuenco ice cap:
 #-------- Initialization --------
 
 # Zenodo repo:
-REPO_URL=https://zenodo.org/record/8333138/files
+REPO_URL=https://zenodo.org/record/8362067/files
 
 # Backup repo:
 # REPO_URL=http://wwwice.lowtem.hokudai.ac.jp/repo/sicopolis/sico_in
