c29ac56159b070b52d75620bdb263152d67e9d74
Shreyas911 <shreyasg911@gmail.com>
2024-02-22 11:55:45 -0600

    CI tests use the header files in test_ad now.

--- test_ad/tests.py
+++ test_ad/tests.py
@@ -4,21 +4,21 @@ import subprocess
 
 
 def test_repo_ant64_bm3_ss25ka_H():
-	subprocess.run (['cp', '../headers/sico_specs_repo_ant64_bm3_ss25ka.h', '../src/sico_specs.h'])
+	subprocess.run (['cp', 'headers/sico_specs_repo_ant64_bm3_ss25ka.h', '../src/sico_specs.h'])
 	subprocess.run('python3 tapenade_config.py -head repo_ant64_bm3_ss25ka -dom ant -jsf inputs.json', shell = True, check = True)
 
 def test_repo_ant64_b2_future09_ctrl_H():
-	subprocess.run (['cp', '../headers/sico_specs_repo_ant64_b2_future09_ctrl.h', '../src/sico_specs.h'])
+	subprocess.run (['cp', 'headers/sico_specs_repo_ant64_b2_future09_ctrl.h', '../src/sico_specs.h'])
 	subprocess.run('python3 tapenade_config.py -head repo_ant64_b2_future09_ctrl -dom ant -delta 5.e-2 -jsf inputs.json', shell = True, check = True)
 
 def test_repo_grl16_bm5_ss25ka_H():	
-	subprocess.run (['cp', '../headers/sico_specs_repo_grl16_bm5_ss25ka.h', '../src/sico_specs.h'])
+	subprocess.run (['cp', 'headers/sico_specs_repo_grl16_bm5_ss25ka.h', '../src/sico_specs.h'])
 	subprocess.run('python3 tapenade_config.py -jsf inputs.json', shell = True, check = True)
 
 def test_repo_ant64_bm3_ss25ka_q_geo():
-	subprocess.run (['cp', '../headers/sico_specs_repo_ant64_bm3_ss25ka.h', '../src/sico_specs.h'])
+	subprocess.run (['cp', 'headers/sico_specs_repo_ant64_bm3_ss25ka.h', '../src/sico_specs.h'])
 	subprocess.run('python3 tapenade_config.py -head repo_ant64_bm3_ss25ka -dom ant -iv q_geo -jsf inputs.json', shell = True, check = True)
 
 def test_repo_grl16_bm5_ss25ka_q_geo():
-    subprocess.run (['cp', '../headers/sico_specs_repo_grl16_bm5_ss25ka.h', '../src/sico_specs.h'])
+    subprocess.run (['cp', 'headers/sico_specs_repo_grl16_bm5_ss25ka.h', '../src/sico_specs.h'])
     subprocess.run('python3 tapenade_config.py -iv q_geo -jsf inputs.json', shell = True, check = True)
