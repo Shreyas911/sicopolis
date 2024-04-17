0a080fa21a7642b8776828f08ad03e38b037ebfc
Ralf Greve <greve@lowtem.hokudai.ac.jp>
2024-04-17 17:37:01 +0900

    Module 'general/sico_init_m.F90': Compatibility checks (DX vs. IMAX, JMAX) added for NPI.

--- src/sicopolis.F90
+++ src/sicopolis.F90
@@ -5,7 +5,7 @@
 !
 #define       MODEL_SICOPOLIS
 #define       VERSION '24'
-#define       DATE    '2024-04-15'
+#define       DATE    '2024-04-17'
 !
 !! Main program of SICOPOLIS.
 !!
--- src/subroutines/general/sico_init_m.F90
+++ src/subroutines/general/sico_init_m.F90
@@ -569,6 +569,41 @@ else
    call error(errormsg)
 end if
 
+#elif (defined(NPI)) /* Northern Patagonian ice field */
+
+if (approx_equal(DX, 0.45_dp, eps_sp_dp)) then
+
+   if ((IMAX /= 182).or.(JMAX /= 314)) then
+      errormsg = ' >>> sico_init: IMAX and/or JMAX wrong!'
+      call error(errormsg)
+   end if
+
+   if ( (.not.(approx_equal(X0,   66.082d0, eps_sp_dp))) &
+        .or. &
+        (.not.(approx_equal(Y0, 4288.950d0, eps_sp_dp))) ) then
+      errormsg = ' >>> sico_init: X0 and/or Y0 wrong!'
+      call error(errormsg)
+   end if
+
+else if (approx_equal(DX, 0.9_dp, eps_sp_dp)) then
+
+   if ((IMAX /=  91).or.(JMAX /= 157)) then
+      errormsg = ' >>> sico_init: IMAX and/or JMAX wrong!'
+      call error(errormsg)
+   end if
+
+   if ( (.not.(approx_equal(X0,   66.082d0, eps_sp_dp))) &
+        .or. &
+        (.not.(approx_equal(Y0, 4288.950d0, eps_sp_dp))) ) then
+      errormsg = ' >>> sico_init: X0 and/or Y0 wrong!'
+      call error(errormsg)
+   end if
+
+else
+   errormsg = ' >>> sico_init: DX wrong!'
+   call error(errormsg)
+end if
+
 #endif /* Different computational domains */
 
 #elif (GRID==2)
